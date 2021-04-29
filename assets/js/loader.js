122c8eaf39c16237c8441bd0f0adff460746fe22
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2021-04-29 16:57:54 +0200

    Refactor code

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,43 +1,46 @@
 const converter = new showdown.Converter();
 
-function unescapeHTML(text)
+class Loader
 {
-    return text.replace( /&amp;/g, "&" )
-               .replace( /&lt;/g, "<" )
-               .replace( /&gt;/g, ">" )
-               .replace( /&quot;/g, "\"")
-               .replace( /&#39;/g, "'" );
-}
-
-function loadMarkdown(data)
-{
-    let item = document.querySelector("[data-markdown]");
-
-    if (item !== undefined)
+    static unescapeHTML(text)
     {
-        const md = unescapeHTML(data);
-        item.innerHTML = converter.makeHtml(md);
+        return text.replace( /&amp;/g, "&" )
+                   .replace( /&lt;/g, "<" )
+                   .replace( /&gt;/g, ">" )
+                   .replace( /&quot;/g, "\"")
+                   .replace( /&#39;/g, "'" );
     }
-}
-
-function loadPage()
-{
-    let url = window.location.href;
-    const file = url.split('/').pop();
-
-    // path
-    if (file !== window.location.host)
+    
+    static loadMarkdown(data)
     {
-        url = url.replace(file, `assets/md/${file}.md`);
+        let item = document.querySelector("[data-markdown]");
+    
+        if (item !== undefined)
+        {
+            const md = unescapeHTML(data);
+            item.innerHTML = converter.makeHtml(md);
+        }
     }
-    else
+    
+    static loadPage()
     {
-        url += "assets/md/index.md";
+        let url = window.location.href;
+        const file = url.split('/').pop();
+    
+        // get filepath
+        if (file !== window.location.host)
+        {
+            url = url.replace(file, `assets/md/${file}.md`);
+        }
+        else
+        {
+            url += "assets/md/index.md";
+        }
+    
+        // load page content
+        fetch(url)
+            .catch(() => { window.location.href = "404.html"; })
+            .then((response) => response.text())
+            .then((data) => loadMarkdown(data));
     }
-
-    // load page
-    fetch(url)
-        .catch(() => { window.location.href = "404.html"; })
-        .then(response => response.text())
-        .then((data) => loadMarkdown(data));
 }
