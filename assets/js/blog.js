f2ae121c66629db2f0552a2b7de21fc1e307ba37
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2022-03-18 18:38:29 +0100

    Make JS code more std-conforming

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,153 +1,110 @@
 "use strict";
 
-class CustomRenderer extends marked.Renderer
-{
-    constructor()
-    {
-        super();
-    }
+class CustomRenderer extends marked.Renderer {
+  constructor() {
+    super();
+  }
 
-    blockquote(quote)
-    {
-        return `<blockquote class="blockquote p-3">${quote}</blockquote>`;
-    }
+  blockquote(quote) {
+    return `<blockquote class="blockquote p-3">${quote}</blockquote>`;
+  }
 
-    table(header, body)
-    {
-        return `<table class="table"><thead">${header}</thead><tbody>${body}</tbody></table>`;
-    }
+  table(header, body) {
+    return `<table class="table"><thead">${header}</thead><tbody>${body}</tbody></table>`;
+  }
 
-    code(code, lang)
-    {
-        const selected = (Prism.languages[lang]) ? lang : "plain";
-        return `<pre><code class="language-${selected}">${code}</code></pre>`;
-    }
+  code(code, lang) {
+    const selected = (Prism.languages[lang]) ? lang : "plain";
+    return `<pre><code class="language-${selected}">${code}</code></pre>`;
+  }
 }
 
-class Loader
-{
-    static getUrl()
-    {
-        const search = window.location.search;
-        return window.location.href.replace(search, "");
+class Loader {
+  convertMarkdown(md) {
+    const options = {
+      "breaks": true,
+      "gfm": true,
+      "renderer": new CustomRenderer(),
+      "xhtml": true
     }
 
-    static convertMarkdown(md)
-    {
-        const options = {
-            "breaks": true,
-            "gfm": true,
-            "renderer": new CustomRenderer(),
-            "xhtml": true
-        }
+    marked.setOptions(options);
+    return marked.parse(md);
+  }
 
-        marked.setOptions(options);
-        return marked.parse(md);
+  sanitizeHtml(html) {
+    const options = {"USE_PROFILES": {"html": true}};
+    return DOMPurify.sanitize(html, options);
+  }
+
+  loadBlogPost(md, id = "blog-content") {
+    const html = this.convertMarkdown(md);
+    const result = this.sanitizeHtml(html);
+    const element = window.document.getElementById(id);
+    element.innerHTML = result;
+  }
+
+  loadBlogEntries(url, routes, id = "blog-entries") {
+    let html = "";
+
+    for (const page of routes) {
+      if (page.visible) {
+        html += `<li><a href="${url}?post=${page.path}">${page.name}</a></li>`;
+      }
     }
 
-    static sanitizeHtml(html)
-    {
-        const options = {
-            "USE_PROFILES": {
-                "html": true
-            }
-        };
-
-        return DOMPurify.sanitize(html, options);
-    }
-    
-    static loadBlogPost(data, id = "blog-content")
-    {
-        let item = window.document.getElementById(id);
-    
-        if (item === undefined)
-        {
-            return;
-        }
-
-        // generate html
-        const html = Loader.convertMarkdown(data);
-        const result = Loader.sanitizeHtml(html);
-
-        // add to page
-        item.innerHTML = result;
-    }
-
-    static loadBlogEntries(data, id = "blog-entries")
-    {
-        let item = window.document.getElementById(id);
-
-        if (item === undefined)
-        {
-            return;
-        }
-
-        // generate entries
-        const url = Loader.getUrl();
-        let html = "";
-
-        for (const item of data)
-        {
-            html += `<li><a href="${url}?post=${item.path}">${item.name}</a></li>`;
-        }
-
-        // add to page
-        const result = Loader.sanitizeHtml(html);
-        item.innerHTML = result;
-    }
+    const result = this.sanitizeHtml(html);
+    const element = window.document.getElementById(id);
+    element.innerHTML = result;
+  }
 }
 
-class Request
-{
-    static async get(url)
-    {
-        const response = await fetch(url);
-        const text = (response.ok) ? await response.text() : "";
-        return text;
-    }
+class Request {
+  async get(url) {
+    const response = await fetch(url);
+    return (response.ok) ? await response.text() : "";
+  }
 }
 
-class Router
-{
-    static async getPage()
-    {
-        // get params
-        const search = window.location.search;
-        const params = new URLSearchParams(search);
-        
-        // get routes
-        const url = Loader.getUrl();
-        const routes = JSON.parse(await Request.get(`${url}assets/routes.json`));
-
-        // get posts
-        const post = params.has("post") ? params.get("post") : "latest";
-        let path = "";
-
-        if (post === "latest")
-        {
-            // use first entry in routes
-            path = routes[0].file;
-        }
-        else
-        {
-            // search for entry in routes
-            for (const item of routes)
-            {
-                if (item.route === post)
-                {
-                    path = item.file;
-                }
-            }
-        }
-
-        // load blog entries
-        Loader.loadBlogEntries(routes);
-
-        // load post
-        const text = await Request.get(`${url}${path}`);
-        Loader.loadBlogPost(text);
-
-        // highlight all code
-        Prism.highlightAll();
+class Router {
+  async getPostPath(name, routes) {
+    if (name === "latest") {
+      // use first entry in routes
+      return routes[0].file;
     }
+
+    // search for entry in routes
+    for (const page of routes) {
+      if (page.path === name) {
+        return page.file;
+      }
+    }
+
+    // post not found
+    return "404";
+  }
+
+  async getPage() {
+    const loader = new Loader();
+    const request = new Request();
+
+    // get url info
+    const search = window.location.search;
+    const url = window.location.href.replace(search, "");
+    const params = new URLSearchParams(search);
+
+    // get route
+    const json = await request.get(`${url}assets/routes.json`);
+    const routes = JSON.parse(json);
+
+    // get page info
+    const pageName = params.has("page") ? params.get("page") : "latest";
+    const pagePath = this.getPostPath(pageName, routes);
+    const pageMd = await request.get(`${url}${pagePath}`);
+
+    // load html
+    loader.loadBlogEntries(url, routes);
+    loader.loadBlogPost(pageMd);
+    Prism.highlightAll();
+  }
 }
