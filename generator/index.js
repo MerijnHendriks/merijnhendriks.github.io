879e847b9574c527eed77d670785308659bc6497
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2022-03-22 11:28:18 +0100

    Refactor generator

--- generator/index.js
+++ generator/index.js
@@ -4,188 +4,155 @@ const showdown = require("showdown");
 const footnotes = require("showdown-ghost-footnotes");
 const { JSDOM } = require("jsdom");
 
-class DOMHelper
-{
-    static getDocument(html)
-    {
-        const dom = new JSDOM(html);
-        return dom.window.document;
-    }
+const url = "https://merijnhendriks.github.io";
 
-    static getHtml(document)
-    {
-        return document.documentElement.outerHTML;
-    }
+/** convert markdown to html */
+function mdToHtml(md)
+{
+    const converter = new showdown.Converter({
+        "extensions": [footnotes],
+        "strikethrough": true,
+        "tables": true,
+        "tasklists": true
+    });
+
+    return converter.makeHtml(md);
 }
 
-class MdConvert
+/** Highlight codeblocks */
+function highlightCode(document)
 {
-    constructor()
+    const codes = document.querySelectorAll("code");
+    
+    for (const element of codes)
     {
-        const options = {
-            "extensions": [footnotes],
-            "strikethrough": true,
-            "tables": true,
-            "tasklists": true
-        };
+        // language name is the first element
+        const lang = element.className.split(" ")[0];
 
-        this.converter = new showdown.Converter(options);
-    }
-
-    run(md)
-    {
-        return this.converter.makeHtml(md);
-    }
-}
-
-class HtmlInjector
-{
-    /** Highlight codeblocks */
-    highlightCode(document)
-    {
-        const codes = document.querySelectorAll("code");
-        
-        for (const element of codes)
+        if (!lang)
         {
-            // language name is the first element
-            const lang = element.className.split(" ")[0];
+            // no language defined
+            continue;
+        }
+        
+        if (!prism.languages[lang])
+        {
+            // install language if it wasnt loaded before
+            require(`prismjs/components/prism-${lang}.js`);
+        }
 
-            if (!lang)
-            {
-                // no language defined
-                continue;
-            }
-            
-            if (!prism.languages[lang])
-            {
-                // install language if it wasnt loaded before
-                require(`prismjs/components/prism-${lang}.js`);
-            }
+        element.classList.remove(lang);
+        prism.highlightElement(element);
+    }
+}
 
+/** Add background to (unformatted) codeblocks */
+function addCodeBackground(document)
+{
+    const codes = document.querySelectorAll("code");
+  
+    for (const element of codes)
+    {
+        // language-* is the first element
+        const lang = element.className.split(" ")[0] || "language-txt";
+
+        if (element.parentElement.tagName === "PRE")
+        {
+            // move language class
+            element.parentElement.classList.add(lang);
             element.classList.remove(lang);
-            prism.highlightElement(element);
-        }
-    }
-
-    /** Add background to (unformatted) codeblocks */
-    addCodeBackground(document)
-    {
-        const codes = document.querySelectorAll("code");
-      
-        for (const element of codes)
-        {
-            // language-* is the first element
-            const lang = element.className.split(" ")[0] || "language-txt";
-
-            if (element.parentElement.tagName === "PRE")
-            {
-                // move language class
-                element.parentElement.classList.add(lang);
-                element.classList.remove(lang);
-            }
-        }
-    }
-
-    /** Add bootstrap styling to blockquotes */
-    addBlockquoteStyling(document)
-    {
-        const blockquotes = document.querySelectorAll("blockquote");
-
-        for (const element of blockquotes)
-        {
-            element.classList.add("blockquote", "px-3");
-        }
-    }
-
-    /** Add bootstrap styling to tables */
-    addTableStyling(document)
-    {
-        const tables = document.querySelectorAll("table");
-
-        for (const element of tables)
-        {
-            element.classList.add("table", "table-bordered");
-        }
-    }
-
-    /** Remove the emoji from the footnote refernces */
-    removeFootnoteBackrefs(document)
-    {
-        const backrefs = document.getElementsByClassName("footnote-backref");
-        
-        while (backrefs.length > 0)
-        {
-            backrefs[0].parentNode.removeChild(backrefs[0]);
         }
     }
 }
 
-class BlogInjector
+/** Add bootstrap styling to blockquotes */
+function addBlockquoteStyling(document)
 {
-    constructor()
+    const blockquotes = document.querySelectorAll("blockquote");
+
+    for (const element of blockquotes)
     {
-        this.url = "https://merijnhendriks.github.io";
-        this.mdConverter = new MdConvert();
-    }
-
-    /** Insert the article into the template */
-    addBlogArticle(html, page)
-    {
-        const md = fs.readFileSync(`./pages/${page.file}`).toString();
-        const article = this.mdConverter.run(md);
-        return html.replace("<!-- __REPLACEME-BLOG-ARTICLE__ -->", article);
-    }
-
-    /** Insert the sidebar archive into the template */
-    addBlogArchive(html, pages)
-    {
-        let items = "";
-
-        for (const page of pages)
-        {
-            const path = (pages[0].name !== page.name) ? page.path : "index.html";
-
-            if (page.visible)
-            {
-                items += `<li><a href="${this.url}/${path}">${page.name}</a></li>`;
-            }
-        }
-
-        return html.replace("<!-- __REPLACEME-BLOG-ARCHIVE__ -->", items);
+        element.classList.add("blockquote", "px-3");
     }
 }
 
-class HtmlGenerator
+/** Add bootstrap styling to tables */
+function addTableStyling(document)
 {
-    constructor()
+    const tables = document.querySelectorAll("table");
+
+    for (const element of tables)
     {
-        const json = fs.readFileSync("./pages/index.json");
-        this.pages = JSON.parse(json);
-        this.blogInjector = new BlogInjector();
-        this.htmlInjector = new HtmlInjector();
+        element.classList.add("table", "table-bordered");
+    }
+}
+
+/** Remove the emoji from the footnote refernces */
+function removeFootnoteBackrefs(document)
+{
+    const backrefs = document.getElementsByClassName("footnote-backref");
+    
+    while (backrefs.length > 0)
+    {
+        backrefs[0].parentNode.removeChild(backrefs[0]);
+    }
+}
+
+/** Insert the article into the template */
+function addBlogArticle(html, page)
+{
+    const md = fs.readFileSync(`./pages/${page.file}`).toString();
+    const article = mdToHtml(md);
+    return html.replace("<!-- __REPLACEME-BLOG-ARTICLE__ -->", article);
+}
+
+function getPagePath(pages, page)
+{
+    return (pages[0].name !== page.name) ? page.path : "index.html";
+}
+
+/** Insert the sidebar archive into the template */
+function addBlogArchive(html, pages)
+{
+    let items = "";
+
+    for (const page of pages)
+    {
+        const path = getPagePath(pages, page);
+
+        if (page.visible)
+        {
+            items += `<li><a href="${url}/${path}">${page.name}</a></li>`;
+        }
     }
 
-    run()
+    return html.replace("<!-- __REPLACEME-BLOG-ARCHIVE__ -->", items);
+}
+
+function main()
+{
+    const json = fs.readFileSync("./pages/index.json");
+    const pages = JSON.parse(json);
+
+    for (const page of pages)
     {
-        for (const page of this.pages)
-        {
-            let html = fs.readFileSync("./templates/base.html").toString();
-            html = this.blogInjector.addBlogArticle(html, page);
-            html = this.blogInjector.addBlogArchive(html, this.pages);
+        let html = fs.readFileSync("./templates/base.html").toString();
+        html = addBlogArchive(html, pages);
+        html = addBlogArticle(html, page);
 
-            const document = DOMHelper.getDocument(html);
-            this.htmlInjector.highlightCode(document);
-            this.htmlInjector.addCodeBackground(document);
-            this.htmlInjector.addBlockquoteStyling(document);
-            this.htmlInjector.addTableStyling(document);
-            this.htmlInjector.removeFootnoteBackrefs(document);
+        const dom = new JSDOM(html);
+        const document = dom.window.document;
+        highlightCode(document);
+        addCodeBackground(document);
+        addBlockquoteStyling(document);
+        addTableStyling(document);
+        removeFootnoteBackrefs(document);
 
-            const name = (this.pages[0].name !== page.name) ? page.path : "index.html";
-            const result = DOMHelper.getHtml(document);
-            fs.writeFileSync(`../${name}`, result);
-        }
+        const path = getPagePath(pages, page);
+        const result = document.documentElement.outerHTML;
+        fs.writeFileSync(`../${path}`, result);
     }
 }
 
 // run the code
-new HtmlGenerator().run();
+main();
